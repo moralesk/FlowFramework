40b9fcead65aa84f64dd2771188daae0cc5ec38f
moralesk <kmorales529@gmail.com>
2024-08-13 12:07:06 -0400

    Initial Commit

---
+++ FlowFramework/FlowFrameworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlowFrameworkApp.swift
+//  FlowFramework
+//
+//  Created by Kelly Morales on 8/13/24.
+//
+
+import SwiftUI
+
+@main
+struct FlowFrameworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
